6e80cea6bbdb7135862189ed770b9c455730c578
cherry16-git <joserri16@gmail.com>
2026-07-15 18:04:17 +0200

    Tabla de usuarios actualizada. Los datos se recogen de base de datos y no de array en código

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,10 @@
 import { createClient } from '@supabase/supabase-js'
 
-// Estos datos te los dará Supabase en los ajustes de tu proyecto (Project Settings > API)
+/* Estos datos te los dará Supabase en los ajustes de tu proyecto (Project Settings > API)
 const supabaseUrl = 'https://xngrbiwhdcyyagwvnzvq.supabase.co'
-const supabaseAnonKey = 'sb_publishable_8IVP8S9sRzMiA6A8ROGj_Q_6mLNllgt'
+const supabaseAnonKey = 'sb_publishable_8IVP8S9sRzMiA6A8ROGj_Q_6mLNllgt'*/
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
